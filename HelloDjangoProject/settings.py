a3485eec221d6a0f62ba15b6616d3de3710be2e6
Taoweidong <taowd@outlook.com>
2023-02-08 22:32:48 +0800

    修改数据库连接为mysql

--- HelloDjangoProject/settings.py
+++ HelloDjangoProject/settings.py
@@ -87,9 +87,24 @@ WSGI_APPLICATION = "HelloDjangoProject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',  # 默认
+        'NAME': 'test',  # 连接的数据库
+        'HOST': '127.0.0.1',  # mysql的ip地址
+        'PORT': 3306,  # mysql的端口
+        'USER': 'root',  # mysql的用户名
+        'PASSWORD': 'root'  # mysql的密码
     }
+}
+
+# 缓存配置： https://blog.z0ukun.com/?p=3135
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',  # redis默认有16个库，后面数字可设定使用几号库
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        }
+    },
 }
 
 # Password validation
